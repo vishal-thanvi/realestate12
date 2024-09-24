ed6e6419c35bc86a0e23669bcf05d1061f4b6b61
vishal thanvi <vishalthanviphalodi@gmail.com>
2024-09-24 15:50:01 +0530

    changes6

--- src/app/about/testimonials/Testimonial.jsx
+++ src/app/about/testimonials/Testimonial.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from 'react';
 import React from 'react';
-import Image from 'next/image'; // Import the Image component
+import Image from 'next/image'; 
 import './Testimonial.css'; 
 
 const Testimonial = () => {
@@ -44,25 +44,24 @@ const Testimonial = () => {
                             <div className="p-2 md:w-2/3 w-full bg-white border-gray-400">
                                 <div className="h-full pt-[10px] md:pt-[30px] pl-[75px] pr-[35px] rounded">
                                     <div className={`transition-transform duration-300 ${animation}`}>
-                                        {click && (
+                                        {click ? (
                                             <div>
-                                                <p className="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
+                                                <p className="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90&apos;s microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
                                                 <a className="inline-flex items-center">
                                                     <Image 
                                                         alt="testimonial"
                                                         src="/images/client-1.png" 
                                                         className="w-[25%] shadow-2xl h-[50%] rounded-full flex-shrink-0 object-cover object-center"
-                                                        width={100} // Set appropriate width
-                                                        height={100} // Set appropriate height
+                                                        width={100}
+                                                        height={100} 
                                                     />
                                                     <span className="flex-grow flex flex-col pl-4">
                                                         <span className="title-font font-bold text-[15px] pb-1 text-gray-900">Alex Meli</span>
-                                                        <span className="text-gray-700 text-[14px]">Ui Designer & CEO</span>
+                                                        <span className="text-gray-700 text-[14px]">Ui Designer &amp; CEO</span>
                                                     </span>
                                                 </a>
                                             </div>
-                                        )}
-                                        {!click && (
+                                        ) : (
                                             <div>
                                                 <p className="leading-relaxed mb-6">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni porro laborum reprehenderit temporibus maiores. Ipsa inventore, harum illo in accusantium nihil id odio rem omnis voluptates optio impedit nemo est.</p>
                                                 <a className="inline-flex items-center">
@@ -70,8 +69,8 @@ const Testimonial = () => {
                                                         alt="testimonial"
                                                         src="/images/client-2.png" 
                                                         className="w-[26%] h-[52%] rounded-full flex-shrink-0 object-cover object-center"
-                                                        width={100} // Set appropriate width
-                                                        height={100} // Set appropriate height
+                                                        width={100} 
+                                                        height={100} 
                                                     />
                                                     <span className="flex-grow flex flex-col pl-4">
                                                         <span className="title-font font-bold text-[15px] pb-1 text-gray-900">Maria Denny</span>
@@ -82,24 +81,22 @@ const Testimonial = () => {
                                         )}
                                     </div>
                                     <div className='flex pb-3 gap-3'>
-                                        <button className="flex ml-auto text-gray border-0 py-2 px-4 bg-gray-200 focus:outline-none hover:bg-gray-400 rounded-full">
+                                        <button className="flex ml-auto text-gray border-0 py-2 px-4 bg-gray-200 focus:outline-none hover:bg-gray-400 rounded-full" onClick={change2}>
                                             <Image 
                                                 src="/images/white-arrow.png" 
                                                 alt="" 
                                                 className="h-9 rotate-180 w-6" 
-                                                width={24} // Set appropriate width
-                                                height={24} // Set appropriate height
-                                                onClick={change2} 
+                                                width={24} 
+                                                height={24} 
                                             />
                                         </button>
-                                        <button className="flex text-gray border-0 py-2 px-4 bg-blue-900 focus:outline-none hover:bg-gray-200 rounded-full">
+                                        <button className="flex text-gray border-0 py-2 px-4 bg-blue-900 focus:outline-none hover:bg-gray-200 rounded-full" onClick={change1}>
                                             <Image 
                                                 src="/images/white-arrow.png" 
                                                 alt="" 
                                                 className="h-9 w-6" 
-                                                width={24} // Set appropriate width
-                                                height={24} // Set appropriate height
-                                                onClick={change1} 
+                                                width={24} 
+                                                height={24} 
                                             />
                                         </button>
                                     </div>
